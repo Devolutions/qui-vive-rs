061df2dec5f27704671ecb8a0c8159e0f348fd81
Marc-André Moreau <mamoreau@devolutions.net>
2018-04-24 12:04:44 -0400

    qui-vive: improve cli+env usage

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ use regex::Regex;
 use url::{Url};
 
 use std::sync::{Arc, Mutex};
+use std::env;
 
 header! { (QuiViveDstUrl, "QuiVive-DstUrl") => [String] }
 header! { (QuiViveIdParam, "QuiVive-IdParam") => [String] }
@@ -48,14 +49,6 @@ struct QuiViveEntry {
     id: String,
     val: String,
     url: String,
-}
-
-#[derive(Clone)]
-struct QuiViveConfig {
-    external_url: String,
-    listener_url: String,
-    redis_hostname: Option<String>,
-    redis_password: Option<String>,
 }
 
 struct QuiVive {
@@ -272,35 +265,76 @@ impl Service for QuiVive {
     }
 }
 
+#[derive(Clone)]
+struct QuiViveConfig {
+    external_url: String,
+    listener_url: String,
+    redis_hostname: Option<String>,
+    redis_password: Option<String>,
+}
+
+impl QuiViveConfig {
+
+    pub fn new() -> Self {
+        QuiViveConfig {
+            external_url: "".to_string(),
+            listener_url: "".to_string(),
+            redis_hostname: None,
+            redis_password: None,
+        }
+    }
+
+    fn load_cli(&mut self) {
+        let yaml = load_yaml!("cli.yml");
+        let app = App::from_yaml(yaml);
+        let matches = app.get_matches();
+
+        self.listener_url = matches.value_of("listener-url").unwrap_or("127.0.0.1:8080").to_string();
+        self.external_url = matches.value_of("external-url").unwrap_or(self.listener_url.as_ref()).to_string();
+
+        let redis_hostname = if let Some(value) = matches.value_of("redis-hostname") {
+            Some(String::from(value))
+        } else {
+            None
+        };
+
+        let redis_password = if let Some(value) = matches.value_of("redis-password") {
+            Some(String::from(value))
+        } else {
+            None
+        };
+
+        self.redis_hostname = redis_hostname;
+        self.redis_password = redis_password;
+    }
+
+    fn load_env(&mut self) {
+        if let Ok(val) = env::var("EXTERNAL_URL") {
+            self.external_url = Some(val).unwrap();
+        }
+
+        if let Ok(val) = env::var("LISTENER_URL") {
+            self.listener_url = Some(val).unwrap();
+        }
+
+        if let Ok(val) = env::var("REDIS_HOSTNAME") {
+            self.redis_hostname = Some(val);
+        }
+
+        if let Ok(val) = env::var("REDIS_PASSWORD") {
+            self.redis_password = Some(val);
+        }
+    }
+}
+
 fn main() {
     env_logger::init();
 
-    let yaml = load_yaml!("cli.yml");
-    let app = App::from_yaml(yaml);
-    let matches = app.get_matches();
+    let mut cfg = QuiViveConfig::new();
+    cfg.load_cli();
+    cfg.load_env();
 
-    let listener_url = matches.value_of("listener-url").unwrap_or("127.0.0.1:8080");
-    let external_url = matches.value_of("external-url").unwrap_or(listener_url.as_ref());
-    let address = listener_url.parse().unwrap();
-
-    let redis_hostname = if let Some(value) = matches.value_of("redis-hostname") {
-        Some(String::from(value))
-    } else {
-        None
-    };
-
-    let redis_password = if let Some(value) = matches.value_of("redis-password") {
-        Some(String::from(value))
-    } else {
-        None
-    };
-
-    let cfg = QuiViveConfig {
-        external_url: external_url.to_string(),
-        listener_url: listener_url.to_string(),
-        redis_hostname: redis_hostname,
-        redis_password: redis_password,
-    };
+    let address = cfg.listener_url.parse().unwrap();
 
     let new_service = move || {
 
